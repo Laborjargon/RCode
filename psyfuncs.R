9b41f8dd7c57c5c044c0ae866ee5553c5a4d7197
Laborjargon <128163785+Laborjargon@users.noreply.github.com>
2023-05-07 10:28:06 +0200

    Add files via upload

--- psyfuncs.R
+++ psyfuncs.R
@@ -180,9 +180,9 @@ c2 <- cast(m2,discs ~ variable,mean)
 
 points(c2$discs,c2$answers)
 # psychometric curves for sven's data-----------------------------------------------------------------------------
-# COC_file = read.table("C:/Users/somme/Documents/R/Data/Coc1011.dat")
+COC_file = read.table("C:/Users/somme/Documents/R/Data/Coc1011.dat")
 # depending on working computer:
-COC_file = read.table("C:/Users/Ben/Desktop/RSHIT/Data/COC/Coc1011.dat")
+# COC_file = read.table("C:/Users/Ben/Desktop/RSHIT/Data/COC/Coc1011.dat")
 # split data into four conditions 1: kein Context 2: Launch 4: Pass
 
 coc_no_context = COC_file[COC_file$V9 == 1, ]
@@ -373,19 +373,18 @@ for (i in 1:8) {
 
 # shifted data and normal data --------------------------------------------
 # logistic_fun2 = function (x, p) {(1 + exp(-(p[1]+p[2]*x)))^(-1)}
-n = 20
+# n = 20
 for (i in 1:8) {
   # Get parameter values for current person
   param_vals <- as.numeric(c(fake_person_df$Parameter_Val_1[i], fake_person_df$Parameter_Val_2[i]))
   
-  # Determine proportion of causal reports without shift
   answer_mean <- logistic_fun2(discs, param_vals)
   
-  # Determine proportion of causal reports with shift
+  # with shift
   shift <- rnorm(1, -0.25, 0.05)
   answer_mean_shift <- logistic_fun2_shift(discs, param_vals, shift)
   
-  # Simulate n trials for each disc overlap based on both means
+  # Simulate n trials for each disc overlap based on both
   #answers <- as.vector(rbinom(n * 7, 1, answer_mean))
   answers = as.vector(mapply(rbinom, n, 1, answer_mean))
   #answers_shift <- as.vector(rbinom(n * 7, 1, answer_mean_shift))
@@ -406,15 +405,15 @@ for (i in 1:8) {
 
 # including all 3 conditions ----------------------------------------------
 # how shift: shift = rep(shift_factor, each = n * 7) in df 
-
+n = 20
 for (i in 1:8) {
   # Get parameter values for current person
   param_vals <- as.numeric(c(fake_person_df$Parameter_Val_1[i], fake_person_df$Parameter_Val_2[i]))
   
-  # Determine proportion of causal reports without shift
+  # without shift
   answer_mean <- logistic_fun2(discs, param_vals)
   
-  # Determine proportion of causal reports with shift
+  # with shift
   shift_neg <- rnorm(1, -0.25, 0.05)
   answer_mean_shift_neg <- logistic_fun2_shift(discs, param_vals, shift_neg)
   
@@ -422,7 +421,7 @@ for (i in 1:8) {
   shift_pos <- rnorm(1, 0.25, 0.05)
   answer_mean_shift_pos <- logistic_fun2_shift(discs, param_vals, shift_pos)
   
-  # Simulate n trials for each disc overlap based on both means
+  # Simulate n trials for each disc overlap based on condition
   answers <- as.vector(mapply(rbinom, n, 1, answer_mean))
   answers_shift_neg <- as.vector(mapply(rbinom, n, 1, answer_mean_shift_neg))
   answers_shift_pos <- as.vector(mapply(rbinom, n, 1, answer_mean_shift_pos))
@@ -440,4 +439,18 @@ for (i in 1:8) {
     master_data <- rbind(master_data, person_data)
   }
 }
+master_data$person = as.factor(master_data$person)
+master_data$shift = as.factor(master_data$shift)
+pmf_master_data = quickpsy(master_data,discs,answers,guess=0,lapses=FALSE,prob=0.5,grouping=.c("shift", "person"),fun=logistic_fun2,parini=list(c(1,15),c(-15,-1)), bootstrap = "none")
 
+pmf_master_data$par$par # 9.8 -9.3 -> NA why ?? 
+
+# outsource script 
+# plotten
+library(ggplot2)
+
+ggplot(master_data, aes(x = x, y = y, color = group1)) +
+  geom_line() +
+  facet_grid(. ~ group2)
+# anova 
+# post hoc ttest 3x 
